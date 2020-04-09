0605569ff114710ae77f82510c2310b91544ca78
lijinyang <ljy_1269@163.com>
2020-04-09 15:15:54 +0800

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+import JyReq from './libs/jy-request'
+import JyCache, {imageCache, cacheSize, cleanCache} from './libs/jy-cache'
+module.exports = {
+	JyReq,
+	JyCache,
+	imageCache,
+	cacheSize,
+	cleanCache,
+}
